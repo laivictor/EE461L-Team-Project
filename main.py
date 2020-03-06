c1aa05906ddfea996d7c636a5d756a166fc44d50
Christopher Acosta <chris.acosta@utexas.edu>
2020-03-06 01:17:38 -0600

    switched to using a single country template html file. began styling

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import json
+
 from flask import Flask, render_template
+
 from json2html import *
 
 app = Flask(__name__)
@@ -14,20 +16,18 @@ def home():
 def countries():
     with open('countries.json') as json_file:
         countries = json.load(json_file)
-    
     countries = sorted(countries, key = lambda i: i['country'])
-    for c in countries:
-        f = open('templates/countries/' + c['country'] + '.html', 'wb')
-        wr = '<!DOCTYPE html><html><head><title>Countries</title><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script><script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script><title></title></head><li><a href="countries"> Countries </a></li><body><br><br>' + json2html.convert(json = c) + '<nav class="navbar navbar-inverse navbar-fixed-top navbar-expand-lg justify-content-between"><div class="container-fluid"><div class="navbar-header"><a class="navbar-brand" href="/countries"> Countries </a></div><ul class="nav navbar-nav"><li><a href="/countries"> Back </a></li></body><body></body></html>'
-        f.write(wr.encode())
-        f.close()
     names = [i['country'] for i in countries]
     return render_template(
             'countries.html', names = names)
 
 @app.route('/countries/<string:page_name>/')
 def open_country(page_name):
-    return render_template('countries/%s.html' % page_name)
+    with open('countries.json') as json_file:
+        countries = json.load(json_file)
+    tb = [i for i in countries if i['country']==page_name]
+    table = json2html.convert(json = tb) #,table_attributes="class=\"datatable\""
+    return render_template('countries_template.html', table = table)
 
 @app.route('/sports')
 def sports():
