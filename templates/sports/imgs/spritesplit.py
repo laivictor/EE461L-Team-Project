4f16221ecc32a1525e79392c3df055ee1b76ad28
laivictor <laivictor2718@gmail.com>
2020-03-08 11:47:42 -0500

    deleting old files

--- templates/sports/imgs/spritesplit.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-
-sheet = Image.open("sportssprite.png")
-
-for x in range(70):
-    a = (x + 1) * 100 - 4
-    icon = sheet.crop((0, a-96, 96, a))
-    
-    icon.save("{}.png".format(x))
